976afa5762e278ebd4bdafb2991a2875f2771947
Shih_Kuan <cliff1304@gmail.com>
2018-02-03 11:18:46 +0800

    Add admin routes, controller and view index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 前台 routes
+  resources :restaurants, only: [:index, :show]
+  root "restaurants#index"
+
+  # 後台 routes
+  namespace :admin, path: "akjdhkajsdh" do
+    resources :restaurants
+    root "restaurants#index"
+  end
 end
