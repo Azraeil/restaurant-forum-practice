b364ce42d3edae0605a4650ce9a8784d20d0d98e
Shih_Kuan <cliff1304@gmail.com>
2018-02-03 12:56:35 +0800

    Create devise user model and devise user authentication for login.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class RestaurantsController < ApplicationController
+  # for login athentication by devise
+  before_action :authenticate_user!
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # 前台 routes
   resources :restaurants, only: [:index, :show]
   root "restaurants#index"
