d1d2bb352c47c6741e22d65f8b2587769c76853f
Shih_Kuan <cliff1304@gmail.com>
2018-02-03 15:48:30 +0800

    Set up admin login authentication method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # for authenticate_admin method check role column value
+  def admin?
+    return self.role == "admin"
+  end
 end
