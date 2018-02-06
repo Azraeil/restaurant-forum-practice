3449ecb6747e36c29f21fe63e47c4ada4d5abc94
Shih_Kuan <cliff1304@gmail.com>
2018-02-06 23:07:49 +0800

    Install Kaminari for paging. Use paging in index page.

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,7 +5,9 @@ class Admin::RestaurantsController < ApplicationController
   before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
-    @restaurants = Restaurant.all
+    # @restaurants = Restaurant.all
+    # use Kaminari
+    @restaurants = Restaurant.page(params[:page]).per(10)
   end
 
   def new
