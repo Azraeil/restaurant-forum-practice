816520233b5d71c2a10e810d0b90a14073577729
Shih_Kuan <cliff1304@gmail.com>
2018-02-06 20:04:32 +0800

    Refactor the view of new.html.erb and edit.html.erb to _form.html.erb . Update show method and add edit, update, destroy method of controller for admin.

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -2,6 +2,8 @@ class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
 
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
@@ -22,11 +24,36 @@ class Admin::RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
+    # 將此段程式碼另外宣告成 private method : find_restaurant
+    # @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "Restaurant data was successfully updated."
+      redirect_to admin_restaurant_path(@restaurant.id)
+    else
+      flash.now[:alert] = "Restaurant was failed to update!"
+      render :edit
+    end
+  end
+
+  def destroy
+    flash[:alert] = "#{@restaurant.name} was deleted!"
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
   end
 
   private
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
