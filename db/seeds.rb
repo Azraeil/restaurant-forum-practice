e6282c24d1812e53651247e91667520b34d88912
Shih_Kuan <cliff1304@gmail.com>
2018-02-08 16:16:17 +0800

    Create seed data for category.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Category seed data
+Category.destroy_all
+
+category_list = [
+  { name: "台式料理" },
+  { name: "中式料理" },
+  { name: "日本料理" },
+  { name: "墨西哥料理" },
+  { name: "美式料理" },
+  { name: "越南料理" },
+  { name: "複合式料理" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+end
+
+puts "Category seed data created!"
+
+# Default admin account
+User.create(email: "admin@test.com", password: "123123", role: "admin")
+puts "Default admin account created!"
