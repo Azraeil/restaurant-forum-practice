917b3cf0cedb914baad599f5f39d915d2019dda6
Shih_Kuan <cliff1304@gmail.com>
2018-02-12 19:32:25 +0800

    Create comment model. Setup relation between restaurant, user and comment.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,9 @@ class Restaurant < ApplicationRecord
   # A restaurant belongs to specific category.
   belongs_to :category
 
+  # A restaurant has_many comments.
+  has_many :comments
+
   # 設定 name 欄位爲必填
   validates_presence_of :name
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # User has_many comments.
+  has_many :comments
+
   # for authenticate_admin method check role column value
   def admin?
     return self.role == "admin"
