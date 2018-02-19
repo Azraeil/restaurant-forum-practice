bce9e2c1f520106437c5d5c3a3151504c763dab6
Shih_Kuan <cliff1304@gmail.com>
2018-02-19 15:53:15 +0800

    Add users routes. Add users controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action find_user, only: [:show, :edit, :update]
+
+  def show
+    # find_user
+  end
+
+  def edit
+    # find_user
+  end
+
+  def update
+    # find_user
+    if @user.update(user_params)
+      flash[:notice] = "User data was successfully updated."
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:alert] = "User was failed to update!"
+      render :edit
+    end
+  end
+
+  private
+  # for before_action
+  def find_user
+    @user = User.find(params[:id])
+    return @user
+  end
+
+  # for strong parameters
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # 透過 devise 實現使用者註冊，登入，管理
   devise_for :users
+
   # 前台 routes
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
   root "restaurants#index"
 
   resources :categories, only: [:show]
+
+  resources :users, only: [:show, :edit, :update]
 
   # 後台 routes
   namespace :admin, path: "akjdhkajsdh" do
