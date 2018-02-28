53621efafeb10b84fac02117268a5d7dd31c03d2
Shih_Kuan <cliff1304@gmail.com>
2018-02-28 10:22:29 +0800

    Add favortied_count column and set default vaule = 0 to restaurants table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227013453) do
+ActiveRecord::Schema.define(version: 20180228020211) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20180227013453) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "tel"
@@ -43,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180227013453) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "category_id"
+    t.integer "favorites_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
